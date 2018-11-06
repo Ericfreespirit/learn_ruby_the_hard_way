9da3a1c51e945b5b9e7b56b1cf2822df2e2cdb2a
freespirit <eric_ling@hotmail.fr>
2018-11-06 17:21:09 +0100

    ex6 - ex7

---
+++ ex_5_more_variables_and_printing/README.rb
@@ -0,0 +1,11 @@
+- A string is a chain of character :
+$"" or $''
+
+- Variable in a string:
+#{}
+
+- Calcul a variable:
+a = 5
+
+b = a * 2
+  = 10
---
+++ ex_5_more_variables_and_printing/ex5.rb
@@ -0,0 +1,22 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+centimeters = height * 2.54 # 187.96
+kilograms = weight * 0.45 # 81.0
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# convert inches to centimeters and pounds to kilograms
+puts "My height in centimeters is #{centimeters} and my weight in kilograms is #{kilograms}"
